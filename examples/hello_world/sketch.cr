236ca6e9b9070c42a5101afb91b7b6de8a18768b
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-06 19:08:36 -0600

    Add initial background method

--- examples/hello_world/sketch.cr
+++ examples/hello_world/sketch.cr
@@ -1,6 +1,9 @@
 require "../../src/processing"
 
 class Sketch < Processing::Sketch
+  def draw
+    background(51)
+  end
 end
 
 Sketch.run
--- src/processing/sketch.cr
+++ src/processing/sketch.cr
@@ -21,5 +21,14 @@ module Processing
     def no_loop
       @loop = false
     end
+
+    def background(rgb : UInt8, alpha : UInt8 = 255)
+      background(rgb, rgb, rgb, alpha)
+    end
+
+    def background(v1 : UInt8, v2 : UInt8, v3 : UInt8, alpha : UInt8 = 255)
+      renderer.draw_color = SDL::Color[v1, v2, v3, alpha]
+      renderer.clear
+    end
   end
 end
