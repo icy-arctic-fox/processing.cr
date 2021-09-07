be16bb2a4d8fe4de6f71636c02e24be50c0cd49b
Postmodern <postmodern.mod3@gmail.com>
2021-09-06 20:59:58 -0700

    Add some documentation and link to https://processing.org/reference/#color.

--- src/processing/core/color.cr
+++ src/processing/core/color.cr
@@ -1,10 +1,19 @@
 module Processing
   module Core
+    #
+    # Implements the [Processing Color API](https://processing.org/reference/#color).
+    #
     module Color
+      #
+      # Sets the background color to a grayscale color.
+      #
       def background(rgb : UInt8, alpha : UInt8 = 255)
         background(rgb, rgb, rgb, alpha)
       end
 
+      #
+      # Sets the background color.
+      #
       def background(v1 : UInt8, v2 : UInt8, v3 : UInt8, alpha : UInt8 = 255)
         renderer.draw_color = SDL::Color[v1, v2, v3, alpha]
         renderer.clear
