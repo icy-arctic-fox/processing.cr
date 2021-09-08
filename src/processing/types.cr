e5402649e7c374b795137abef363c1378b12123c
Postmodern <postmodern.mod3@gmail.com>
2021-09-07 19:12:59 -0700

    Add type aliases for Processing 4.x compatibility.

--- src/processing/types.cr
+++ src/processing/types.cr
@@ -1,3 +1,19 @@
 module Processing
   alias Byte = UInt8
+
+  alias ArrayList = Array
+
+  alias FloatDict = Hash(String,Float)
+
+  alias FloatList = Array(Float)
+
+  alias HashMap = Hash
+
+  alias IntDict = Hash(String,Int)
+
+  alias IntList = Array(Int)
+
+  alias StringDict = Hash(String,String)
+
+  alias StringList = Array(String)
 end
