0e6ec168d45f8a24a1e2f59406f335c4a2577f92
Postmodern <postmodern.mod3@gmail.com>
2021-09-09 08:47:02 -0700

    Stub out Core::Shape#create_shape.

--- src/processing/core/shape.cr
+++ src/processing/core/shape.cr
@@ -3,6 +3,24 @@ require "./shape/primitives_2d"
 module Processing
   module Core
     module Shape
+      enum ShapeType
+        Ellipse
+        Rect
+        Arc
+        Triangle
+        Sphere
+        Box
+        Quad
+        Line
+      end
+
+      def create_shape
+        not_implemented!
+      end
+
+      def create_shape(type : ShapeType, *points)
+        not_implemented!
+      end
     end
   end
 end
